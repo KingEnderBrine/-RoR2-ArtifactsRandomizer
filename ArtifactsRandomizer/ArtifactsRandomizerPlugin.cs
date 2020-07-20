23970f4d8cdb28ef976ae963e7e2c3a86c05b191
Юрасов Денис <d.yurasov@powwwer.io>
2020-07-20 13:43:49 +0700

    Mod release 1.2.0

--- ArtifactsRandomizer/ArtifactsRandomizerPlugin.cs
+++ ArtifactsRandomizer/ArtifactsRandomizerPlugin.cs
@@ -2,6 +2,7 @@
 using BepInEx.Configuration;
 using R2API.Utils;
 using RoR2;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -9,11 +10,39 @@ namespace ArtifactsRandomizer
 {
     [R2APISubmoduleDependency(nameof(CommandHelper))]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.KingEnderBrine.ArtifactsRandomizer", "Artifacts Randomizer", "1.1.0")]
+    [BepInPlugin("com.KingEnderBrine.ArtifactsRandomizer", "Artifacts Randomizer", "1.2.0")]
     public class ArtifactsRandomizerPlugin : BaseUnityPlugin
     {
         private static ConfigWrapper<bool> isEnabled { get; set; }
         private static ConfigWrapper<string> blacklist { get; set; }
+        private static ConfigWrapper<string> artifactWeights { get; set; }
+        private static ConfigWrapper<int> maxCount { get; set; }
+
+        private static WeightedSelection<ArtifactIndex> artifactWeightsSelection {
+            get
+            {
+                var selection = new WeightedSelection<ArtifactIndex>();
+                var customWeights = new Dictionary<ArtifactIndex, int>();
+                for (var i = 0; i < ArtifactCatalog.artifactCount; i++)
+                {
+                    customWeights[(ArtifactIndex)i] = 50;
+                }
+                artifactWeights.Value
+                    .Split(new string[] { ", ", "," }, System.StringSplitOptions.RemoveEmptyEntries)
+                    .Select(el => el.Split(new char[] { ':', ' ' }, System.StringSplitOptions.RemoveEmptyEntries))
+                    .Select(el =>
+                    {
+                        var index = ArtifactCatalog.FindArtifactIndex(el[0]);
+                        customWeights[index] = int.Parse(el[1]);
+                        return index;
+                    }).ToArray();
+                foreach(var row in customWeights)
+                {
+                    selection.AddChoice(row.Key, row.Value);
+                }
+                return selection;
+            }
+        }
 
         private static ArtifactIndex[] blackListIndex => blacklist.Value
             .Split(new char[] { ',', ' ' }, System.StringSplitOptions.RemoveEmptyEntries)
@@ -27,7 +56,11 @@ namespace ArtifactsRandomizer
 
             isEnabled = Config.Wrap("Main", "enabled", "Is mod should randomize artifacts or not", true);
 
-            blacklist = Config.Wrap("Main", "blacklist", "Artifact names (comma separated) that should be ingored when randomizing", "");
+            blacklist = Config.Wrap("Main", "blacklist", "Artifact names (comma-separated) that should be ingored when randomizing", "");
+
+            artifactWeights = Config.Wrap("Main", "artifactWeights", "Artifact weight with names (comma-separated).\nExample: `Bomb: 20, Command: 50`", "");
+            
+            maxCount = Config.Wrap("Main", "maxCount", "Maximum count of artifacts to be enabled via randomization", -1);
 
             On.RoR2.Run.Start += (orig, self) =>
             {
@@ -48,18 +81,21 @@ namespace ArtifactsRandomizer
             {
                 return;
             }
-            var range = Enumerable.Range(0, ArtifactCatalog.artifactCount).Select(el => (ArtifactIndex)el).ToList();
-            range = range.Except(blackListIndex).ToList();
-            var enabledCount = Random.Range(0, range.Count + 1);
+            var selection = artifactWeightsSelection;
+            foreach (var index in blackListIndex.OrderByDescending(el => el))
+            {
+                selection.RemoveChoice((int)index);
+            }
+            var enabledCount = Random.Range(0, Mathf.Max(1, Mathf.Min(selection.Count + 1, maxCount.Value == -1 ? selection.Count + 1 : maxCount.Value + 1)));
             for (var i = 1; i <= enabledCount; i++)
             {
-                var index = Random.Range(0, range.Count);
-                RunArtifactManager.instance.SetArtifactEnabledServer(ArtifactCatalog.GetArtifactDef(range[index]), true);
-                range.RemoveAt(index);
+                var index = selection.EvaluteToChoiceIndex(Random.Range(0F, 1F));
+                RunArtifactManager.instance.SetArtifactEnabledServer(ArtifactCatalog.GetArtifactDef(selection.GetChoice(index).value), true);
+                selection.RemoveChoice(index);
             }
-            for (var i = 0; i < range.Count; i++)
+            for (var i = 0; i < selection.Count; i++)
             {
-                RunArtifactManager.instance.SetArtifactEnabledServer(ArtifactCatalog.GetArtifactDef(range[i]), false);
+                RunArtifactManager.instance.SetArtifactEnabledServer(ArtifactCatalog.GetArtifactDef(selection.GetChoice(i).value), false);
             }
         }
 
--- README.md
+++ README.md
@@ -12,14 +12,31 @@ Randomizes artifacts on every stage
 # Blacklist
 You can add artifacts that will not be randomized. If you enable artifacts that are blacklisted, they will be enabled for the whole run.
 To blacklist artifacts you should edit `blacklist` entry in config file `BepInEx/config/com.KingEnderBrine.ArtifactsRandomizer.cfg`.
-It should be equal to comma separated artifact names.
+It should be equal to comma-separated artifact names.
 
 Example:
 ```
 blacklist = FriendlyFire, TeamDeath, WeakAssKnees, WispOnDeath
 ```
 
-#### List of artifact names
+# Artifact weights
+You can add weight for specific artifacts it will affect artifact enabling chance.
+To do that you should edit `artifactWeights` entry in config file `BepInEx/config/com.KingEnderBrine.ArtifactsRandomizer.cfg`.
+It should be equal to comma-separated entries that are like this pattern: `{Artifact name}: {weight}`.
+Artifacts that are not included have a default weight of 50.
+
+Example:
+```
+artifactWeights = Bomb: 20, Command: 75, Glass: 30
+```
+
+# Maximum artifacts count
+You can set the maximum count of artifacts that will be enabled.
+To do that you should edit `maxCount` entry in config file `BepInEx/config/com.KingEnderBrine.ArtifactsRandomizer.cfg`.
+If you set it to `-1` then all artifacts could be enabled at once
+
+***
+# List of artifact names
 Must be used exact names from that list (for vanilla, modded artifacts could also be added)
 
 * `Chaos` - FriendlyFire
@@ -56,6 +73,12 @@ Must be used exact names from that list (for vanilla, modded artifacts could als
 
 ***
 # Changelog
+
+**1.2.0**
+
+* Added maximum count of randomized artifacts
+
+* Added weights config, so you can change artifact enabling chance
 
 **1.1.0**
 
